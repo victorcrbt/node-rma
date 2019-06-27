726f20b036a75c02d9b6c37a72e9cc30b15ea73e
Victor Batalha <victor@Victors-iMac.local>
2019-06-26 21:34:58 -0300

    Adicionado rotas para cadstro de usuário e autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+// Controllers
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.use("/", (req, res) => {
-  return res.send("Olá, mundo!");
-})
+// Rotas sem autenticação
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
