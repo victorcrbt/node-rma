9070b81ff1b5485fe83a1c1d1cf89ddb8679d10e
Victor Batalha <madivcb@gmail.com>
2019-06-27 10:14:38 -0300

    Validação da entrada de dados no login

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as yup from 'yup';
 
 import jwtConfig from '../../config/jwt';
 
@@ -6,6 +7,34 @@ import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const validationSchema = yup.object().shape({
+      email: yup.string().required("O e-mail não pode estar em branco."),
+      password: yup.string().required("A senha não pode estar em branco.")
+    });
+
+    // Validação dos campos
+    try {
+      // Procura por erros na entrada de dados. Se não houver, prossegue com o código.
+      await validationSchema.validate(req.body, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = [];
+
+      // Lista todos os erros e insere no array o campo e a mensagem de cada um.
+      err.inner.map(error => {
+        const infos = {
+          field: error.path,
+          msg: error.message,
+        };
+
+        errors.push(infos);
+      });
+
+      // Se existir qualque erro de validação, retorna em formato json com o campo e a mensagem.
+      return res.status(400).json(errors);
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
