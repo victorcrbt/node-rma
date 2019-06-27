520ce1298c27e56da05c2696d6cb6972f36cf798
Victor Batalha <victor@Victors-iMac.local>
2019-06-26 21:45:41 -0300

    Adicionado middleware para rotas que precisam de autenticação

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+// Middlewares
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // Rotas sem autenticação
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
