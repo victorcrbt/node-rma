b04a37d2ea1c89a060f4bf6cb90a105d2031c9c2
Victor Batalha <madivcb@gmail.com>
2019-06-27 11:28:54 -0300

    Criado método para atualização do cadastro e suas validações. Alterado o formato de retorno dos erros de validação de entrada de dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,7 +12,7 @@ class UserController {
         .required('O e-mail é obrigatório.'),
       password: yup
         .string()
-        .min(6, 'A senha deve conter pelo menos 6 dígitos.')
+        .min(6, 'A senha deve conter pelo menos 6 caracteres.')
         .required('A senha é obrigatória.'),
     });
 
@@ -29,18 +29,14 @@ class UserController {
       err.inner.map(error => {
         const infos = {
           field: error.path,
-          msg: error.message,
+          error: error.message,
         };
 
         errors.push(infos);
       });
 
       // Se existir qualque erro de validação, retorna em formato json com o campo e a mensagem.
-      return res.status(400).json(errors);
-    }
-
-    if (!(await validationSchema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Falha na validação dos dados.' });
+      return res.status(400).json({ error: errors });
     }
 
     const { email } = req.body;
@@ -68,6 +64,94 @@ class UserController {
       employee,
       salesman,
       client,
+      reference_id,
+    });
+  }
+
+  async update(req, res) {
+    const validationSchema = yup.object().shape({
+      name: yup.string(),
+      email: yup.string().email('Formato de e-mail inválido.'),
+      oldPassword: yup.string(),
+      password: yup
+        .string()
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword
+            ? field
+                .required('A senha é obrigatória.')
+                .min(6, 'A senha deve conter pelo menos 6 caracteres')
+            : field
+        ),
+      confirmPassword: yup
+        .string()
+        .when('password', (password, field) =>
+          password
+            ? field
+                .required('A confirmação de senha é obrigatória.')
+                .oneOf([yup.ref('password')], 'As senhas não coincidem.')
+            : field
+        ),
+    });
+
+    // Validação dos campos
+    try {
+      // Procura por erros na entrada de dados. Se não houver, prossegue com o código.
+      await validationSchema.validate(req.body, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = [];
+
+      // Lista todos os erros e insere no array o campo e a mensagem de cada um.
+      err.inner.map(error => {
+        const infos = {
+          field: error.path,
+          error: error.message,
+        };
+
+        errors.push(infos);
+      });
+
+      // Se existir qualque erro de validação, retorna em formato json com o campo e a mensagem.
+      return res.status(400).json({ error: errors });
+    }
+
+    const { password, oldPassword, email } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    // Se o e-mail for enviad, verifica se o novo já está em uso
+    if (email && user.email !== email) {
+      const emailInUse = await User.findOne({ where: { email } });
+
+      if (emailInUse) {
+        return res
+          .status(400)
+          .json({ error: 'O e-mail informado já está em uso.' });
+      }
+    }
+
+    // Verifica se o usuário tentou enviar a senha sem enviar a antiga
+    if (password && !oldPassword) {
+      return res
+        .status(401)
+        .json({ error: 'Você deve informar a senha antiga para prosseguir.' });
+    }
+
+    // Verifica se a senha antiga foi enviada, e se sim, se bate com a do banco de dados
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res
+        .status(401)
+        .json({ error: 'A senha antiga informada está incorreta.' });
+    }
+
+    const { name, admin, reference_id } = await user.update(req.body);
+
+    return res.status(200).json({
+      msg: 'Usuário atualizado com sucesso!',
+      name,
+      email,
+      admin,
       reference_id,
     });
   }
