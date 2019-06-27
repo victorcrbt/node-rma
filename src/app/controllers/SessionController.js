5722f4b25f8478f5ad226ce0c00974d22b79b666
Victor Batalha <victor@Victors-iMac.local>
2019-06-26 21:36:03 -0300

    Criado método para autenticação

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+import jwtConfig from '../../config/jwt';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    console.log('ok');
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciais inválidas.' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Credenciais inválidas.' });
+    }
+
+    const { id, name, reference_id } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+        reference_id,
+      },
+      token: jwt.sign({ id }, jwtConfig.secret, {
+        expiresIn: jwtConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
