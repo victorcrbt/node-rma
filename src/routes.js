d81c1a108e5c4da8e9b69d0facfe06acd59dcc85
Victor Batalha <victor@Victors-iMac.local>
2019-06-27 21:33:02 -0300

    Criação da rota para envio de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 // Controllers
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // Rotas sem autenticação
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.post('/files', upload.single('file'), FileController.store);
+// Rotas que necessitam autenticação
 routes.use(authMiddleware);
 
-// Rotas que necessitam autenticação
 routes.put('/users', UserController.update);
 
 export default routes;
