8d1dbf5ef1f18ada443be32c41509830fd8f4f7f
Victor Batalha <victor@Victors-iMac.local>
2019-06-27 21:32:43 -0300

    Criado método para fazer o relacionamento com outras tabelas

--- src/database/index.js
+++ src/database/index.js
@@ -2,12 +2,13 @@ import Sequelize from 'sequelize';
 
 // Importação dos models
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 // Configuração do banco de dados
 import dbConfig from '../config/database';
 
 // Models
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -17,7 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
