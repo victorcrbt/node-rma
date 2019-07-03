d080d62c8ae4038a779c4b155d1e16d65e1cfbf6
Victor Batalha <madivcb@gmail.com>
2019-07-03 17:41:47 -0300

    Criado método para visualização e exclusão de um funcionário

--- src/app/controllers/EmployeeController.js
+++ src/app/controllers/EmployeeController.js
@@ -19,6 +19,27 @@ class EmployeeController {
     return res.status(200).json(employees);
   }
 
+  async show(req, res) {
+    /**
+     * Verifica se o usuário é administrador ou funcionário comum.
+     */
+    const { admin, employee } = await User.findByPk(req.userId);
+
+    if (!admin && !employee) {
+      return res.status(401).json({ error: 'Você não tem permissão para realizar esta ação.' });
+    }
+
+    const emp = await Employee.findByPk(req.params.id, {
+      attributes: ['id', 'name', 'document'],
+    });
+
+    if (!emp) {
+      return res.status(404).json({ error: 'Não foi encontrado um funcionário com o ID informado.' });
+    }
+
+    return res.status(200).json(emp);
+  }
+
   async store(req, res) {
     /**
      * Verifica se o usuário é administrador ou funcionário comum.
@@ -45,6 +66,27 @@ class EmployeeController {
     const { id, name, document } = await Employee.create(req.body);
 
     return res.status(201).json({ id, name, document });
+  }
+
+  async delete(req, res) {
+    /**
+     * Verifica se o usuário é administrador ou funcionário comum.
+     */
+    const { admin, employee } = await User.findByPk(req.userId);
+
+    if (!admin && !employee) {
+      return res.status(401).json({ error: 'Você não tem permissão para realizar esta ação.' });
+    }
+
+    const emp = await Employee.findByPk(req.params.id);
+
+    if (!emp) {
+      return res.status(404).json({ error: 'Não foi encontrado um funcionário com o ID informado.' });
+    }
+
+    emp.destroy();
+
+    return res.status(200).json({ msg: 'Funcionário deletado com sucesso.' });
   }
 }
 
