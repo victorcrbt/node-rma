fecff241668d9decbb6e502af81eea6cc2e0b794
Victor Batalha <madivcb@gmail.com>
2019-07-03 17:31:55 -0300

    Criado método para listagem dos funcionários

--- src/app/controllers/EmployeeController.js
+++ src/app/controllers/EmployeeController.js
@@ -2,6 +2,23 @@ import Employee from '../models/Employee';
 import User from '../models/User';
 
 class EmployeeController {
+  async index(req, res) {
+    /**
+     * Verifica se o usuário é administrador ou funcionário comum.
+     */
+    const { admin, employee } = await User.findByPk(req.userId);
+
+    if (!admin && !employee) {
+      return res.status(401).json({ error: 'Você não tem permissão para realizar esta ação.' });
+    }
+
+    const employees = await Employee.findAll({
+      attributes: ['id', 'name', 'document']
+    })
+
+    return res.status(200).json(employees);
+  }
+
   async store(req, res) {
     /**
      * Verifica se o usuário é administrador ou funcionário comum.
