f37c8a8d53163c7cdcbe154fa26f1b930111c329
Victor Batalha <madivcb@gmail.com>
2019-07-03 14:50:15 -0300

    Criado model Brand

---
+++ src/app/models/Brand.js
@@ -0,0 +1,16 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Brand extends Model {
+  static init(sequelize) {
+    super.init({
+      id: Sequelize.INTEGER,
+      descrption: Sequelize.STRING,
+    }, {
+      sequelize,
+    })
+
+    return this;
+  }
+}
+
+export default Brand;
