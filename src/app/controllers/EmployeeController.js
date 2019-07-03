ed533c1dbe6c93c9161d85e3648ba30e84c4fbe6
Victor Batalha <madivcb@gmail.com>
2019-07-03 17:28:23 -0300

    Criado método para criação de funcionário

---
+++ src/app/controllers/EmployeeController.js
@@ -0,0 +1,34 @@
+import Employee from '../models/Employee';
+import User from '../models/User';
+
+class EmployeeController {
+  async store(req, res) {
+    /**
+     * Verifica se o usuário é administrador ou funcionário comum.
+     */
+    const { admin, employee } = await User.findByPk(req.userId);
+
+    if (!admin && !employee) {
+      return res.status(401).json({ error: 'Você não tem permissão para realizar esta ação.' });
+    }
+
+    /**
+     * Verifica se já existe um funcionário com o documento informado.
+     */
+    const documentExists = await Employee.findOne({
+      where: {
+        document: req.body.document
+      }
+    })
+
+    if (documentExists) {
+      return res.status(400).json({ error: `O documento já está em uso pelo funcionário ${documentExists.id}.` })
+    }
+
+    const { id, name, document } = await Employee.create(req.body);
+
+    return res.status(201).json({ id, name, document });
+  }
+}
+
+export default new EmployeeController();
