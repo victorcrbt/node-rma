9283b782252d0266c91542df496346254c37c555
Victor Batalha <madivcb@gmail.com>
2019-07-03 14:38:47 -0300

    Nenhuma alteração

--- src/database/migrations/20190627182618-populate-status.js
+++ src/database/migrations/20190627182618-populate-status.js
@@ -1,40 +1,39 @@
-'use strict';
+
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-
-      return queryInterface.bulkInsert('status', [
-        {
-          description: 'Pendente',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-        {
-          description: 'Enviado para fábrica',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-        {
-          description: 'Enviado para assistência',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-        {
-          description: 'Aguardando pagamento',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-        {
-          description: 'Aguardando retirada',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-        {
-          description: 'Entregue',
-          created_at: "NOW()",
-          updated_at: "NOW()",
-        },
-      ]);
+    return queryInterface.bulkInsert('status', [
+      {
+        description: 'Pendente',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+      {
+        description: 'Enviado para fábrica',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+      {
+        description: 'Enviado para assistência',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+      {
+        description: 'Aguardando pagamento',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+      {
+        description: 'Aguardando retirada',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+      {
+        description: 'Entregue',
+        created_at: "NOW()",
+        updated_at: "NOW()",
+      },
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
