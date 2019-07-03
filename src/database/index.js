a35949521b7ca3a5fce019ae3109a8151e721ba5
Victor Batalha <madivcb@gmail.com>
2019-07-03 14:47:34 -0300

    Adição do model WarrantyTypes

--- src/database/index.js
+++ src/database/index.js
@@ -4,12 +4,13 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Status from '../app/models/Status';
+import WarrantyType from '../app/models/WarrantyType';
 
 // Configuração do banco de dados
 import dbConfig from '../config/database';
 
 // Models
-const models = [User, File, Status];
+const models = [User, File, Status, WarrantyType];
 
 class Database {
   constructor() {
