810ba2a1afcbbff47f36858235d3bebbea436782
Victor Batalha <madivcb@gmail.com>
2019-07-04 15:54:32 -0300

    Criado rotas para listagem e criação de cliente

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ import BrandController from './app/controllers/BrandController';
 import FileController from './app/controllers/FileController';
 import EmployeeController from './app/controllers/EmployeeController';
 import SalesmanController from './app/controllers/SalesmanController';
+import ClientController from './app/controllers/ClientController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -20,37 +21,70 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 // Rotas sem autenticação
+/**
+ * Usuários
+ */
 routes.post('/users', UserController.store);
 
+/**
+ * Sessão
+ */
 routes.post('/sessions', SessionController.store);
 
 // Rotas que necessitam autenticação
 routes.use(authMiddleware);
 
+/**
+ * Usuários
+ */
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
 
+/**
+ * Status
+ */
 routes.get('/status', StatusController.index);
 
+/**
+ * Tipos de garantia
+ */
 routes.get('/warranty_types', WarrantyTypeController.index);
 
+/**
+ * Marcas
+ */
 routes.get('/brands', BrandController.index);
 routes.post('/brands', BrandController.store);
 routes.put('/brands/:id', BrandController.update);
 routes.delete('/brands/:id', BrandController.delete);
 
+/**
+ * Funcionários
+ */
 routes.get('/employees', EmployeeController.index);
 routes.get('/employees/:id', EmployeeController.show);
 routes.post('/employees', EmployeeController.store);
 routes.put('/employees/:id', EmployeeController.update);
 routes.delete('/employees/:id', EmployeeController.delete);
 
+/**
+ * Representantes
+ */
 routes.get('/salesmen', SalesmanController.index);
 routes.get('/salesmen/:id', SalesmanController.show);
 routes.post('/salesmen', SalesmanController.store);
 routes.put('/salesmen/:id', SalesmanController.update);
 routes.delete('/salesmen/:id', SalesmanController.delete);
 
+/**
+ * Clientes
+ */
+routes.get('/clients', ClientController.index);
+routes.post('/clients', ClientController.store);
+
+/**
+ * Arquivos
+ */
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
