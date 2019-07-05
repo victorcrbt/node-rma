a47ed6551a0ae1366c1b05d1689c0d5710ad21d4
Victor Batalha <madivcb@gmail.com>
2019-07-05 16:12:39 -0300

    Adicionado rota para editar um produto

--- src/routes.js
+++ src/routes.js
@@ -92,6 +92,7 @@ routes.delete('/clients/:id', ClientController.delete);
 routes.get('/products', ProductController.index);
 routes.get('/products/:id', ProductController.show);
 routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
 routes.delete('/products/:id', ProductController.delete);
 
 /**
