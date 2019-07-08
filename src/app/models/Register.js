be77477267a79875b788f562d9f37bccb884baa4
Victor Batalha <victor@Victors-iMac.local>
2019-07-07 21:27:10 -0300

    Removido associações de 'brand_id' e 'salesman_id'

--- src/app/models/Register.js
+++ src/app/models/Register.js
@@ -6,11 +6,9 @@ class Register extends Model {
       {
         user_id: Sequelize.INTEGER,
         client_id: Sequelize.INTEGER,
-        salesman_id: Sequelize.INTEGER,
         warranty_type_id: Sequelize.INTEGER,
         status_id: Sequelize.INTEGER,
         product_id: Sequelize.INTEGER,
-        brand_id: Sequelize.INTEGER,
         entry_invoice: Sequelize.INTEGER,
         entry_date: Sequelize.DATE,
         delivery_cost: Sequelize.FLOAT,
@@ -32,17 +30,12 @@ class Register extends Model {
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.Client, { foreignKey: 'client_id', as: 'client' });
-    this.belongsTo(models.Salesman, {
-      foreignKey: 'salesman_id',
-      as: 'salesman',
-    });
     this.belongsTo(models.WarrantyType, {
       foreignKey: 'warranty_type_id',
       as: 'warranty_type',
     });
     this.belongsTo(models.Status, { foreignKey: 'status_id', as: 'status' });
     this.belongsTo(models.Product, { foreignKey: 'product_id', as: 'product' });
-    this.belongsTo(models.Brand, { foreignKey: 'brand_id', as: 'brand' });
   }
 }
 
