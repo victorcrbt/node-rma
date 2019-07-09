2d00725ef0f73aa3fc0f96eb943a24adbdfe0103
Victor Batalha <madivcb@gmail.com>
2019-07-09 17:52:24 -0300

    feature: método para sincronizar marcas

---
+++ src/app/controllers/SyncBrandController.js
@@ -0,0 +1,49 @@
+import Odbc from '../../lib/Odbc';
+
+import Brand from '../models/Brand';
+
+class SyncProductController {
+  async sync(req, res) {
+    const response = await Odbc.query('SELECT * FROM "02568s000"."MARCAS_GARANTIA"');
+
+    response.map(async ({ id, description }) => {
+      const brand = await Brand.findByPk(id);
+
+      if (brand && (brand.description !== description)) {
+        await brand.update({
+          description
+        })
+      } else if (!brand) {
+        await Brand.create({
+          id,
+          description
+        })
+      }
+    })
+
+    return res.status(201).json();
+  }
+
+  async resync(req, res) {
+    const response = await Odbc.query('SELECT * FROM "02568s000"."MARCAS_GARANTIA"');
+
+    response.map(async ({ id, description }) => {
+      const brand = await Brand.findByPk(id);
+
+      if (brand && (brand.description !== description)) {
+        await brand.update({
+          description
+        })
+      } else if (!brand) {
+        await Brand.create({
+          id,
+          description
+        })
+      }
+    })
+
+    return;
+  }
+}
+
+export default new SyncProductController();
