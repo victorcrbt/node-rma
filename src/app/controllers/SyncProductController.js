e99c98f9b141972d21fecb7d7d4f5489e40c1ee4
Victor Batalha <madivcb@gmail.com>
2019-07-11 15:06:28 -0300

    feature: finalizado método para sincronizar produtos

--- src/app/controllers/SyncProductController.js
+++ src/app/controllers/SyncProductController.js
@@ -3,177 +3,82 @@ import Odbc from '../../lib/Odbc';
 import Product from '../models/Product';
 import Brand from '../models/Brand';
 
-import SyncBrandController from './SyncBrandController';
-
 class SyncProductController {
   async sync(req, res) {
-    const saveProductsToDatabase = async products => {
-      console.log('func 4');
-
-      if (products) {
-
-        try {
-          await products.map(async ({ id, brand_id, description, unit, ncm }) => {
-            try {
-              const product = await Product.findByPk(id);
-
-              console.log(product);
-
-              // if (product && product.description !== description) {
-              //   try {
-              //     await product.update({
-              //       description,
-              //     });
-              //   } catch (err) {
-              //     console.log(err);
-              //   }
-              // } else if (!product) {
-              //   try {
-              //     Product.create({
-              //       id,
-              //       brand_id,
-              //       description,
-              //       unit,
-              //       ncm
-              //     });
-              //   } catch (err) {
-              //     console.log(err);
-              //   }
-
-              // }
-            } catch (err) {
-              console.log(err);
-            }
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      }
-
+    /**
+     * Pegar as marcas do ERP.
+     */
+    const getBrandsFromErp = () => {
+      return Odbc.query('SELECT * FROM "02568s000"."MARCAS_GARANTIA"');
     };
 
-    const getProductsFromErp = async () => {
-      try {
-        const products = await Odbc.query(
-          'SELECT * FROM "02568s000"."PRODUTOS_GARANTIA"'
-        );
-
-        if (products) {
-          return console.log(products);
-        }
-      } catch (err) {
-        return console.log(err);
-      }
-    };
-
+    /**
+     * Percorer a lista de marcas pegas do ERP.
+     * Verificar se existe no banco de dados. Se sim, atualizar, caso contrário, salvar.
+     */
     const saveBrandsToDatabase = async brands => {
-      console.log('func 2');
+      return Promise.all(
+        brands.map(async ({ id, description }) => {
+          const brand = await Brand.findByPk(id);
 
-      if (brands) {
-
-        try {
-          await brands.map(async ({ id, description }) => {
-            try {
-              const brand = await Brand.findByPk(id);
-
-              if (brand && brand.description !== description) {
-                try {
-                  await brand.update({
-                    description,
-                  });
-                } catch (err) {
-                  console.log(err);
-                }
-              } else if (!brand) {
-                try {
-                  Brand.create({
-                    id,
-                    description,
-                  });
-                } catch (err) {
-                  console.log(err);
-                }
-
-              }
-            } catch (err) {
-              console.log(err);
-            }
-          });
-
-          return getProductsFromErp();
-        } catch (err) {
-          console.log(err);
-        }
-      }
-
+          if (brand && brand.description !== description) {
+            await brand.update({
+              description,
+            });
+          } else if (!brand) {
+            await Brand.create({
+              id,
+              description,
+            });
+          }
+        })
+      );
     };
 
-    const getBrandsFromErp = async () => {
-      try {
-        const brands = await Odbc.query(
-          'SELECT * FROM "02568s000"."MARCAS_GARANTIA"'
-        );
-
-        if (brands) {
-          return saveBrandsToDatabase(brands);
-        }
-      } catch (err) {
-        return console.log(err);
-      }
+    /**
+     * Pegar os produtos do ERP.
+     */
+    const getProductsFromErp = () => {
+      return Odbc.query('SELECT * FROM "02568s000"."PRODUTOS_GARANTIA"');
     };
 
+    /**
+     * Percorer a lista de produtos pegas do ERP.
+     * Verificar se existe no banco de dados. Se sim, atualizar, caso contrário, salvar.
+     */
+    const saveProductsToDatabase = async products => {
+      return Promise.all(
+        products.map(async ({ id, brand_id, description, unit, ncm }) => {
+          const product = await Product.findByPk(id);
 
+          if (product && product.description !== description) {
+            await product.update({
+              description,
+            });
+          } else if (!product) {
+            await Product.create({
+              id,
+              brand_id: brand_id === 0 ? 98 : brand_id,
+              description,
+              unit,
+              ncm,
+            });
+          }
+        })
+      );
+    };
 
-    getBrandsFromErp();
-    // saveBrandsToDatabase();
-    // getProductsFromErp();
+    /**
+     * As funções precisam ser executadas nesta ordem e a próxima só pode executar quando a anteriro tiver terminado.
+     */
+    const brands = await getBrandsFromErp(); // Pega as marcas do ERP.
+    await saveBrandsToDatabase(brands); // Passa as marcas para serem percorridas e salvas no banco de dados.
+    const products = await getProductsFromErp(); // Pega as marcas do ERP.
+    await saveProductsToDatabase(products); // Passa as marcas para serem percorridas e salvas no banco de dados.
 
-    // const resyncBrands = async () => {
-
-    //   if (response) {
-    //     response.map(async ({ id, description }) => {
-    //       const brand = await Brand.findByPk(id);
-
-    //       if (brand && brand.description !== description) {
-    //         await brand.update({
-    //           description,
-    //         });
-    //       } else if (!brand) {
-    //         await Brand.create({
-    //           id,
-    //           description,
-    //         });
-    //       }
-    //     });
-    //   }
-    // };
-
-    // const syncToErp = async () => {
-    //   const products = await Odbc.query(
-    //     'SELECT * FROM "02568s000"."PRODUTOS_GARANTIA"'
-    //   );
-
-    //   if (products) {
-    //     products.map(async ({ id, brand_id, description, unit, ncm }) => {
-    //       const product = await Product.findByPk(id);
-
-    //       if (product && product.description !== description) {
-    //         await product.update({
-    //           description,
-    //         });
-    //       } else if (!product) {
-    //         await Product.create({
-    //           id,
-    //           brand_id: brand_id === 0 ? 98 : brand_id,
-    //           description,
-    //           unit,
-    //           ncm,
-    //         });
-    //       }
-    //     });
-    //   }
-    // };
-
+    /**
+     * A resposta só pode ocorrer quando todos as funções tiverem terminado.
+     */
     return res.status(201).json();
   }
 }
