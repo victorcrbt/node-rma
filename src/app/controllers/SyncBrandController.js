14bcbd4b92d4c18ca0a9ba835acb7a8be420ecc8
Victor Batalha <madivcb@gmail.com>
2019-07-11 15:05:59 -0300

    feature: funcionalização do controller e encadeamento de promises

--- src/app/controllers/SyncBrandController.js
+++ src/app/controllers/SyncBrandController.js
@@ -4,47 +4,40 @@ import Brand from '../models/Brand';
 
 class SyncProductController {
   async sync(req, res) {
-    const response = await Odbc.query(
-      'SELECT * FROM "02568s000"."MARCAS_GARANTIA"'
-    );
+    /**
+     * Pegar as marcas do ERP.
+     */
+    const getBrandsFromErp = () => {
+      return Odbc.query('SELECT * FROM "02568s000"."MARCAS_GARANTIA"');
+    };
 
-    response.map(async ({ id, description }) => {
-      const brand = await Brand.findByPk(id);
+    /**
+     * Percorer a lista de marcas pegas do ERP.
+     * Verificar se existe no banco de dados. Se sim, atualizar, caso contrário, salvar.
+     */
+    const saveBrandsToDatabase = async brands => {
+      return Promise.all(
+        brands.map(async ({ id, description }) => {
+          const brand = await Brand.findByPk(id);
 
-      if (brand && brand.description !== description) {
-        await brand.update({
-          description,
-        });
-      } else if (!brand) {
-        await Brand.create({
-          id,
-          description,
-        });
-      }
-    });
+          if (brand && brand.description !== description) {
+            await brand.update({
+              description,
+            });
+          } else if (!brand) {
+            await Brand.create({
+              id,
+              description,
+            });
+          }
+        })
+      );
+    };
+
+    const brands = await getBrandsFromErp();
+    await saveBrandsToDatabase(brands);
 
     return res.status(201).json();
-  }
-
-  async resync(req, res) {
-    const response = await Odbc.query(
-      'SELECT * FROM "02568s000"."MARCAS_GARANTIA"'
-    );
-
-    response.map(async ({ id, description }) => {
-      const brand = await Brand.findByPk(id);
-
-      if (brand && brand.description !== description) {
-        await brand.update({
-          description,
-        });
-      } else if (!brand) {
-        await Brand.create({
-          id,
-          description,
-        });
-      }
-    });
   }
 }
 
